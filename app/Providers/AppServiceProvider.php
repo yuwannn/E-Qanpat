aea90bf2ec067501629d17b4b62a6e870c705658
Yuwan <yuwandriy123@gmail.com>
2025-12-03 21:08:03 +0700

    membenarkan url agar js bisa digunakan di https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Paginator::useBootstrapFive();
+
+        // 2. TAMBAHKAN KODE INI:
+        // Jika aplikasi berjalan di environment 'production' (Railway), paksa pakai HTTPS
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
